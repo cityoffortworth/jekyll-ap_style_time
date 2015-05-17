e7d05fbefd276d3a0fe9fc2c329db6762942fee6
Greg Scott <i@gregoryjscott.com>
2015-05-17 17:04:09 -0500

    Implement displaying time
    Still needs more tests, but it's mostly there.

--- lib/jekyll/ap_style_time/ap_time_filter.rb
+++ lib/jekyll/ap_style_time/ap_time_filter.rb
@@ -5,7 +5,32 @@ module Jekyll
     module APTimeFilter
 
       def ap_time(input)
-        input
+        date = DateTime.parse(input)
+
+        month = "#{date.strftime('%b')}."
+        case date.month
+        when 4, 5, 6, 7
+          month = date.strftime('%B')
+        when 9
+          month = 'Sept.'
+        end
+
+        day = date.strftime('%e').strip
+        year = date.strftime('%Y')
+        date_str = "#{month} #{day}, #{year}"
+
+        time = date.strftime('%l:%M').strip
+        meridian = date.strftime('%p').gsub('AM', 'a.m.').gsub('PM', 'p.m.')
+
+        if (date.hour == 0 && date.minute == 0)
+          time_str = 'midnight'
+        elsif (date.hour == 12 && date.minute == 0)
+          time_str = 'noon'
+        else
+          time_str = "#{time} #{meridian}"
+        end
+
+        "#{time_str} #{date_str}"
       end
 
     end
--- test/jekyll/ap_style_time/ap_time_filter_test.rb
+++ test/jekyll/ap_style_time/ap_time_filter_test.rb
@@ -5,9 +5,24 @@ describe Jekyll::APStyleTime::APTimeFilter do
 
   let(:ap_time_filter) { Object.new.extend(Jekyll::APStyleTime::APTimeFilter) }
 
-  it 'does nothing' do
-    new_date = ap_time_filter.ap_time('2015-01-01 00:00:00 -0600')
-    assert_equal '2015-01-01 00:00:00 -0600', new_date
+  it 'displays January correctly' do
+    new_date = ap_time_filter.ap_time('2015-01-10')
+    assert new_date.end_with? 'Jan. 10, 2015'
+  end
+
+  it 'displays noon correctly' do
+    new_date = ap_time_filter.ap_time('2015-01-10 12:00:00 -600')
+    assert_equal 'noon Jan. 10, 2015', new_date
+  end
+
+  it 'displays midnight correctly' do
+    new_date = ap_time_filter.ap_time('2015-01-10 00:00:00 -600')
+    assert_equal 'midnight Jan. 10, 2015', new_date
+  end
+
+  it 'displays non-noon and non-midnight correctly' do
+    new_date = ap_time_filter.ap_time('2015-01-10 14:23:00 -600')
+    assert_equal '2:23 p.m. Jan. 10, 2015', new_date
   end
 
 end
